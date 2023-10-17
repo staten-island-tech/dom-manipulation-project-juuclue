4f55eba3c4e9c41adc92e8d30d58685c58ed6fe0
juuclue <144699439+juuclue@users.noreply.github.com>
2023-10-17 09:30:46 -0400

    hi

---
+++ dom.js
@@ -0,0 +1,12 @@
+const DOMselectors = {
+    button: document.getElementById("btc"),
+
+const text document.querySelector("#form"),
+}
+//    firstName:document.querySelector(".first-name"),
+//    h2s:document.querySelectorAll("h2"),
+//};
+
+//DOMselectors.form.addEventListener("submit", function(event)){
+
+//}
