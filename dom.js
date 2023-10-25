f025c6a75f09cfc4dd64f5a6e9ca209b6c3c38b5
juuclue <144699439+juuclue@users.noreply.github.com>
2023-10-25 10:15:55 -0400

    3

--- dom.js
+++ dom.js
@@ -1,23 +1,38 @@
-const DOMselectors = {
+/*const DOMselectors = {
     bigbootycheeks: document.getElementById("btc"),
-
     cheeks:document.querySelector("#cheeks"),
-
+    purr:document.getElementById("purr"),
     point: document.querySelectorAll(".point"),
 
 //    empty: document.querySelector(".nothing"),
 
 };
-console.log(DOMselectors);
+*/
+//console.log(DOMselectors);
 
-function 
-//const bigbootycheeks = document.getElementById("btc");
-//const text = document.querySelector("#text");
 
-//    firstName:document.querySelector(".first-name"),
-//    h2s:document.querySelectorAll("h2"),
-//};
 
+const insert = document.querySelector("#insert");
+insert.addEventListener("click", () => {
+  const subject = document.querySelector("#subject");
+  const positionSelect = document.querySelector("#position");
+  subject.insertAdjacentHTML(
+    positionSelect.value,
+    "<strong>inserted text</strong>",
+  );
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  document.location.reload();
+});
+const firstname = (prompt("type your firstname"));
+const lastname = (prmpt("type your lastname"));
+console.log(insert);
+
+function fun(){  
+    document.getElementById("myForm").reset();  
+}
 //DOMselectors.form.addEventListener("submit", function(event)){
 
 //}
