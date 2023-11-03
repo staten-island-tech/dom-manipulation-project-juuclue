2577b21ade426464ce6e0e298760aecf1441bbbb
juuclue <144699439+juuclue@users.noreply.github.com>
2023-11-03 10:16:01 -0400

    e

---
+++ jjv2.js
@@ -0,0 +1,20 @@
+function sequence(){
+    let i1 = 0;
+    let i2 = 1;
+    for(let i = 2; i <= F; i++);
+        console.log(i);
+ const seq = i1 + i2;
+ i1 = i2;
+ i2 = seq;
+ console.log(i2);
+ return i2;
+
+
+
+}
+
+/* for( F2 = 1; F3 = 2;);
+let A = a.F2
+let B = b.F3
+return( A + B); */
+
